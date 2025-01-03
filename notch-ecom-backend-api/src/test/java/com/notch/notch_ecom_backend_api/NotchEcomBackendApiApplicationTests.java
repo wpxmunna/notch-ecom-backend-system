370dca27c9e79183fe72a0b373d598b2cede21e7
wpxmunna <wpxmunna@gmail.com>
2025-01-03 16:38:52 +0600

    Initial commit

---
+++ notch-ecom-backend-api/src/test/java/com/notch/notch_ecom_backend_api/NotchEcomBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notch.notch_ecom_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotchEcomBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
