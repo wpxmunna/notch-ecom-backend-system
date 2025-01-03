4096035029fa12d859c0e1a59393df1dab591d06
wpxmunna <wpxmunna@gmail.com>
2025-01-04 00:31:31 +0600

    Add eureka server

--- notch-ecom-backend-api/src/main/java/com/notch/notch_ecom_backend_api/NotchEcomBackendApiApplication.java
+++ notch-ecom-backend-api/src/main/java/com/notch/notch_ecom_backend_api/NotchEcomBackendApiApplication.java
@@ -3,6 +3,7 @@ package com.notch.notch_ecom_backend_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class NotchEcomBackendApiApplication {
 
